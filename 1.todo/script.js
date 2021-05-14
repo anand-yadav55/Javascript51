af5f07c604cb3f28f8bd8e0a065ac9f68c60421f
anand-yadav55 <anand55yadav@gmail.com>
2021-05-15 02:17:57 +0530

    setting remove todo

--- 1.todo/script.js
+++ 1.todo/script.js
@@ -1,14 +1,22 @@
 let todoItems = [];
 
 const todoInput = document.querySelector("#todoInput");
+let removeButtons = document.querySelectorAll(".removeBtn");
 
 function createList() {
   let list = document.createElement("ul");
-  todoItems.forEach((ele) => {
+  todoItems.forEach((ele, idx) => {
     let node = document.createElement("li");
-    let textnode = document.createTextNode(ele);
-    node.appendChild(textnode);
+    let textNode = document.createTextNode(ele);
+    let removeButton = document.createElement("button");
+    let removeButtonTextNode = document.createTextNode("Remove it");
+    removeButton.setAttribute("class", "removeBtn");
+    removeButton.setAttribute("idx", idx);
+    removeButton.appendChild(removeButtonTextNode);
+    node.appendChild(textNode);
+    node.appendChild(removeButton);
     list.appendChild(node);
+    removeButton.addEventListener("click", () => console.log("remom"));
   });
   console.log(list);
   return list;
@@ -21,7 +29,19 @@ function renderTodoList() {
 }
 
 // To remove the element
-function removeTodo() {}
+function removeTodo(ele) {
+  //   console.log(ele);
+}
+function addRemoveFunction() {
+  removeButtons = document.querySelectorAll(".removeBtn");
+
+  //   removeButtons.forEach((ele) =>
+  //     ele.addEventListener("click", () => console.log(ele))
+  //   );
+  for (let i = 0; i < removeButtons.length; i++) {
+    removeButtons[i].addEventListener("click", () => console.log(ele));
+  }
+}
 
 todoInput.addEventListener("submit", (e) => {
   e.preventDefault();
